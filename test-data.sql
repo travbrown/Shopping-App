40a771bb0a3d397b5ff3a9721c26211bd35b5e4a
Travis Brown <travjbrown98@gmail.com>
2020-04-27 05:03:56 -0700

    + insert sql commads

--- test-data.sql
+++ test-data.sql
@@ -1,4 +1,27 @@
 
+INSERT INTO Orders
+  (orderID,custID,orderstatus,ordertotal,orderdate,recipient_name,shipping_addr)
+VALUES
+  (700,	100,	'pending',	300,	2018-9-19,	'Travis ',	'2251 Sherman Avenue'),
+  (711,	111, 'ongoing',	720,	2018-9-19,	'Tina',	'543 Hyman Street'),
+  (722,	122,	'completed',	2400,	2018-7-31, 'Peniel',	'420 Junik lane'),
+  (733,	133,	'pending',	105,	2018-12-24,	'Mekano',	'435 Yuha Ave'),
+  (744,	144,	'ongoing',	5600,	2018-3-14,	'Fiji',	'483 Wedi Circle');
+
+INSERT INTO Sailors
+  (name, age, experience)
+VALUES
+  ('john', 32, 5),
+  ('jane', 22, 3),
+  ('janie', 45, 17);
+INSERT INTO Sailors
+  (name, age, experience)
+VALUES
+  ('john', 32, 5),
+  ('jane', 22, 3),
+  ('janie', 45, 17);
+
+
 INSERT INTO Sailors
   (name, age, experience)
 VALUES
